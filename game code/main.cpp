62ad7b15072c3d7a30c18c28b412c0b146e6d7d3
fahimaAzizi <fahimaAzizi397@gmail.com>
2024-10-16 18:54:02 +0500

    c++ on game

--- game code/main.cpp
+++ game code/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
- 
-void Setup(){
+bool gameOver;
+const int width =20 ;
+const int height =20 ;
+int x,y ,furitX, furitY , score;
+enum eDirecton {STOP =0 ,LEFT , RIGHT ,UP, DOWN};
+eDirecton dir;
 
-
+void Setup()
+ {
+  
+  gameOver = false ;
+  dir = STOP;
+  x = width /2 ;
+  y  = height / 2;
+  furitX =rand()% width ;
+  furitY = rand()% height;
 }
 void Draw()
 {
@@ -19,7 +31,13 @@ void logic()
 }
 int main()
 {
-
+  Setup();
+  while ( !gameOver )
+  {
+    Draw();
+    input();
+    logic();
+  }
 return 0 ;
 }
 
