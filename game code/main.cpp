e10a718c230ca511e1e9cae2a5b61cb42d64de53
fahimaAzizi <fahimaAzizi397@gmail.com>
2024-10-17 12:20:46 +0500

    snak game is going

--- game code/main.cpp
+++ game code/main.cpp
@@ -19,7 +19,35 @@ void Setup()
 }
 void Draw()
 {
+  system("cls"); 
+  for (int i =0 ; i < width+2 ; i++);
+     cout <<"#" ;
+  cout << endl ;
 
+  for (int i =0 ; i < height ; i++)
+  {
+      for (int j =0 ; j < width ; j++)
+      {
+        if (j == 0 )
+          cout << "#";
+          
+        if (i == y && j == x)
+
+          cout <<"0";
+        else if (i == furitY && j == furitX)
+          cout <<"f";
+        else 
+          cout <<" ";
+        if (j == width -1)
+          cout <<"#";
+      }
+      cout << endl ;
+    
+  }
+
+  for (int i =0 ; i < width+2 ; i++);
+    cout <<"#" ;
+  cout << endl ;
 }
 void input()
 {
